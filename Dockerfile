46f159db64cbb23642c6ac8c189d8745ba5ca64f
adrianperez1997 <adrian97@correo.ugr.es>
2020-04-28 20:48:21 +0200

    Feat: add_fisica function. Adds host to inventory file, now in yaml

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,12 @@ ARG DEBIAN_FRONTEND=noninteractive
 RUN apt update &&\
     apt install -y software-properties-common &&\
     apt-add-repository --yes --update ppa:ansible/ansible &&\
-    apt install -y ansible
+    apt install -y ansible &&\
+    apt install -y python3-pip
+
+
+ADD requirements.txt /
+RUN pip3 install -r requirements.txt
 
 RUN mkdir /keys
 
@@ -12,11 +17,8 @@ ADD hosts /etc/ansible/
 ADD /keys/miclave /keys/
 RUN chmod 600 /keys/miclave
 
-ADD requirements.txt /
 ADD setup.yml /
 ADD main3.py /
 
-
-#RUN pip3 install -r requirements.txt
 #CMD ["ansible', '-m','ping','localhost']
 CMD [ "python3", "./main3.py" ]
--- main3.py
+++ main3.py
@@ -1,4 +1,41 @@
 import subprocess
+import yaml
 
-r = subprocess.run(['ansible','-m','ping','fisica'])
-print(r)
+def setup(ip, port, key, user):
+    o = {'all':{'hosts':{'fisica':{'ansible_host': ip,
+                             'ansible_port': port,
+                             'ansible_user': user,
+                             'ansible_ssh_private_key_file':key,
+                             'ansible_ssh_extra_args': '-o StrictHostKeyChecking=no'}}}}
+    stream = open('prueba.yaml','w')
+    yaml.dump(o,stream)
+
+#setup('172.28.191.232', 22, '/keys/miclave', 'vagrant')
+#r = subprocess.run(['ansible','-m','ping','-i','prueba.yaml','fisica'])
+#r = subprocess.run(['ansible-playbook','setup.yml'])
+#print(r)
+
+
+def add_fisica(name, ip, port, key, user):
+    try:
+        with open('prueba.yaml') as f:
+            o = yaml.load(f, Loader=yaml.FullLoader)
+            if name in o['all']['hosts']:
+                print('Escoje otro nombre')
+    except FileNotFoundError:
+        o={'all':{'hosts':{},'children':{'fisica':{'hosts':{}}}}}
+    finally:
+        o['all']['hosts'][name]={'ansible_host': ip,
+                    'ansible_port': port,
+                    'ansible_user': user,
+                    'ansible_ssh_private_key_file': key,
+                    'ansible_ssh_extra_args': '-o StrictHostKeyChecking=no'}
+
+        o['all']['children']['fisica']['hosts'][name]= None
+        s = open('prueba.yaml','w')
+        yaml.dump(o,s)
+
+    #r = subprocess.run(['ansible', '-m', 'ping', '-i', 'prueba.yaml', 'fisica'])
+
+add_fisica('fisica1','172.28.123.153', 22, '/keys/miclave', 'vagrant')
+add_fisica('fisica2','172.28.191.232', 22, '/keys/miclave', 'vagrant')
